2c4ef1075071bf0267e1e3d5b5c71d8346d016b0
aryawpratama <aryawpratama@gmail.com>
2021-05-15 20:22:58 +0700

    nav fix

--- src/scripts/Components/Navbar/Navbar.js
+++ src/scripts/Components/Navbar/Navbar.js
@@ -7,12 +7,28 @@ class Navbar extends HTMLElement {
     render() {
         this.innerHTML = `
     <nav>
-        <img src="${this.logo}" alt="Logo">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#">Favorites</a></li>
-            <li><a href="https://github.com/aryawpratama">About Us</a></li>
-        </ul>
+        <img class="logo" src="${this.logo}" alt="Logo">
+        <div class="desktop">
+            <ul class="navigation-desktop">
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Favorites</a></li>
+                <li><a href="https://github.com/aryawpratama">About Us</a></li>
+            </ul>
+        </div>
+        <div class="mobile">
+            <div class="bar">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="dropdown">
+                <ul class="navigation-mobile">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="#">Favorites</a></li>
+                    <li><a href="https://github.com/aryawpratama">About Us</a></li>
+                </ul>
+            </div>
+        </div>
     </nav>
     `;
     }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,23 @@
 import "regenerator-runtime";
 import "../styles/main.css";
+import $ from "jquery";
 // Components
 import "./Components/Navbar/Navbar.js";
-import "./Components/Jumbotron/Jumbotron.js"
+import "./Components/Jumbotron/Jumbotron.js";
 // Data
 import data from "../DATA.json";
 const dataJSON = JSON.stringify(data);
 // Render
 const root = document.querySelector(".root");
 const rootRender = () => {
-    root.innerHTML = `
+  root.innerHTML = `
         <nav-bar logo='https://www.ariasydney.com.au/sites/ariasyd/files/Aria_Favicon.png'></nav-bar>
-        <jumbo-tron></jumbo-tron>
     `;
 };
-
 rootRender();
+// Additional
+$(".bar").on("click", function (e) {
+  e.preventDefault();
+  $(this).toggleClass("active");
+  $(".dropdown").toggleClass("down");
+});
