076b8e5ba99acae26a5b8e39143c1df9f508edb7
aryawpratama <aryawpratama@gmail.com>
2021-05-30 14:54:12 +0700

    rev-3

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,12 @@
 import "regenerator-runtime";
 import "../styles/main.css";
 import $ from "jquery";
-import SwipeListener from 'swipe-listener';
+import SwipeListener from "swipe-listener";
 // Components
 import "./Components/Navbar/Navbar.js";
 import "./Components/Jumbotron/Jumbotron.js";
 import "./Components/Content/Content.js";
-import "./Components/Footer/Footer.js"
+import "./Components/Footer/Footer.js";
 // Data
 import data from "../DATA.json";
 const dataJSON = JSON.stringify(data);
@@ -15,7 +15,8 @@ const jumbotronBg = "images/heros/hero-image_2.jpg";
 // Render
 const root = document.querySelector(".root");
 const rootRender = () => {
-    root.innerHTML = `
+  root.innerHTML = `
+    <a href="#explore" class="skipcontent">Skip to Content</a>
         <nav-bar logo='${logo}'></nav-bar>
         <jumbotron-component bg='${jumbotronBg}'></jumbotron-component>
         <content-component data='${dataJSON}'></content-component>
@@ -26,19 +27,19 @@ rootRender();
 // Additional
 const nav = document.querySelector("nav");
 // Button onClick
-$(".bar").on("click", function(e) {
-    e.preventDefault();
-    $(this).toggleClass("active");
-    $(".dropdown").toggleClass("down");
+$(".bar").on("click", function (e) {
+  e.preventDefault();
+  $(this).toggleClass("active");
+  $(".dropdown").toggleClass("down");
 });
 // Document onScroll
-document.addEventListener('scroll', function(e) {
-    $(this).removeClass("down");
-    $(".dropdown").removeClass("down");
-    $(".bar").removeClass("active");
-    if (window.scrollY >= 371) {
-        nav.style.background = "rgba(0, 0, 0, 0.8)";
-    } else {
-        nav.style.background = "rgba(0, 0, 0, 0)";
-    }
-})
+document.addEventListener("scroll", function (e) {
+  $(this).removeClass("down");
+  $(".dropdown").removeClass("down");
+  $(".bar").removeClass("active");
+  if (window.scrollY >= 371) {
+    nav.style.background = "rgba(0, 0, 0, 0.8)";
+  } else {
+    nav.style.background = "rgba(0, 0, 0, 0)";
+  }
+});
