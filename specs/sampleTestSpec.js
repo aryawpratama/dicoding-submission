b555aea1200d33f2646240d19e5640a1c2726635
aryawpratama <aryawpratama@gmail.com>
2021-07-03 16:12:41 +0700

    confusing-automation-test

--- specs/sampleTestSpec.js
+++ specs/sampleTestSpec.js
@@ -1,8 +1,8 @@
-const sum = (a, b) => a + b
-
-describe('A Sample Test for Sum', () => {
-  it('should return a + b value', () => {
-    expect(sum(2, 3))
-      .toEqual(5)
+import '../src/scripts/Components/Detail/DetailComponent'
+describe("Like a restaurant", () => {
+  it("Should show the like button", () => {
+    document.body.innerHTML=`<detail-component></detail-component>`
+    console.log(document.body.innerHTML)
+    expect()
   })
 })
--- src/scripts/Components/Detail/DetailComponent.js
+++ src/scripts/Components/Detail/DetailComponent.js
@@ -5,6 +5,7 @@ import $ from 'jquery'
 import { DBController } from '../../Data/FavouriteIDB'
 import CONFIG from '../../Global/Config'
 import './Detail.css'
+import LikeButton from './LikeButton'
 class Detail extends HTMLElement {
   connectedCallback () {
     this.data = this.getAttribute('data')
@@ -38,34 +39,37 @@ class Detail extends HTMLElement {
     }
   }
 
-  databaseFetch () {
-    DBController.get(this.parse.id).then(res => {
-      if (res !== undefined) {
-        this.indexedDB = res
-        if (this.parse.id === this.indexedDB.id) {
-          $('#favorite').removeClass('favorite')
-          $('#favorite').addClass('favorited')
-        }
-      } else {
-        this.indexedDB = { id: null }
-      }
-    })
-  }
+  // databaseFetch () {
+  //   DBController.get(this.parse.id).then(res => {
+  //     if (res !== undefined) {
+  //       this.indexedDB = res
+  //       if (this.parse.id === this.indexedDB.id) {
+  //         $('#favorite').removeClass('favorite')
+  //         $('#favorite').addClass('favorited')
+  //       }
+  //     } else {
+  //       this.indexedDB = { id: null }
+  //     }
+  //   })
+  // }
 
   favoriteButton () {
-    this.favorite = document.querySelector('#favorite')
-    this.favorite.addEventListener('click', event => {
-      event.preventDefault()
-      if (this.indexedDB.id === this.parse.id) {
-        DBController.delete(this.indexedDB.id)
-        $('#favorite').removeClass('favorited')
-        $('#favorite').addClass('favorite')
-      } else {
-        DBController.add(this.parse)
-        $('#favorite').removeClass('favorite')
-        $('#favorite').addClass('favorited')
-      }
-      this.databaseFetch()
+    // this.favorite = document.querySelector('#favorite')
+    // this.favorite.addEventListener('click', event => {
+    //   event.preventDefault()
+    //   if (this.indexedDB.id === this.parse.id) {
+    //     DBController.delete(this.indexedDB.id)
+    //     $('#favorite').removeClass('favorited')
+    //     $('#favorite').addClass('favorite')
+    //   } else {
+    //     DBController.add(this.parse)
+    //     $('#favorite').removeClass('favorite')
+    //     $('#favorite').addClass('favorited')
+    //   }
+    //   this.databaseFetch()
+    // })
+    LikeButton(this.indexedDB, this.parse, (res) => {
+      this.indexedDB = res
     })
   }
 
