0a16326b95ca39c1df46507170dc2e10ddd12055
aryawpratama <aryawpratama@gmail.com>
2021-07-19 12:59:36 +0700

    fix-e2e-test

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -2,26 +2,26 @@
 Feature('Liking Restaurant')
 
 Before(({ I }) => {
-  I.amOnPage('#/favorite')
+  I.amOnPage('#/')
+  I.seeElement('.card')
+  I.click(locate('.card').first())
 })
 
 Scenario('Show Empty list', ({ I }) => {
+  I.amOnPage('#/favorite')
   I.seeElement('#not-found')
 })
 
 Scenario('Like a Restaurant', ({ I }) => {
-  I.amOnPage('#/detail/rqdv5juczeskfw1e867')
   I.seeElement('fav-button')
   I.click(locate('#favorite').first())
 })
 Scenario('Show Non Empty list', ({ I }) => {
-  I.amOnPage('#/detail/rqdv5juczeskfw1e867')
   I.click(locate('#favorite').first())
   I.amOnPage('#/favorite')
   I.dontSeeElement('#not-found')
 })
 Scenario('Unlike a Restaurant', ({ I }) => {
-  I.amOnPage('#/detail/rqdv5juczeskfw1e867')
   I.seeElement('fav-button')
   I.click(locate('#favorite').first())
   I.seeElement('.favorited')
